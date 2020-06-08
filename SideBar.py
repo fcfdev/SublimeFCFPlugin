5c27f629470120350454a06f7034031594f50b4a
Vladimir Markin <v.o.markin@mail.ru>
2020-06-08 03:09:31 +0300

    Append command Copy file path

--- SideBar.py
+++ SideBar.py
@@ -1,7 +1,31 @@
 import sublime
 import sublime_plugin
 import os
+import sys
 import subprocess
+__dir__ = os.path.dirname(os.path.realpath(__file__));
+sys.path.insert(1, __dir__)
+import pyperclip
+
+
+class FcfGetPathCommand(sublime_plugin.TextCommand):
+  def run(self, edit, **args):  
+    path = None
+
+    try:
+      if len(args["dirs"]) != 0:
+          path = args["dirs"][0]
+      elif len(args["files"]) != 0:
+        path = args["files"][0]
+    except:
+      return
+
+    if path == None:
+      return
+    pyperclip.copy(path)
+
+
+    
 
 class FcfNewTemplateCommand(sublime_plugin.TextCommand):
   directory = None
@@ -9,15 +33,16 @@ class FcfNewTemplateCommand(sublime_plugin.TextCommand):
   template  = None
 
   def run(self, edit, **args):
-    self.directory = ""
-    self.args      = {"file": ""}
-    self.template  = args["template"]
-
-    if len(args["dirs"]) != 0:
-      self.directory = args["dirs"][0]
-    elif len(args["files"]) != 0:
-      self.directory = os.path.dirname(args["files"][0])
-      pass
+    try:
+      self.directory = ""
+      self.args      = {"file": ""}
+      self.template  = args["template"]
+      if len(args["dirs"]) != 0:
+          self.directory = args["dirs"][0]
+      elif len(args["files"]) != 0:
+        self.directory = os.path.dirname(args["files"][0])
+    except:
+      return;
 
     if self.directory == None:
       return
@@ -32,11 +57,11 @@ class FcfNewTemplateCommand(sublime_plugin.TextCommand):
 
 
   def action(self):
-    if len(self.directory) == 0:
-      return
-    if len(self.args["file"]) == 0:
-      return
     try:
+      if len(self.directory) == 0:
+        return
+      if len(self.args["file"]) == 0:
+        return
       subprocess.Popen(["fcfmngr", "c", self.template, self.args["file"]], cwd=self.directory)
     except e:
       print(e)
